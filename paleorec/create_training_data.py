562afff7ffb87231647361feffa37e0742612806
shrav95 <shravya.maety@siesgst.ac.in>
2021-02-07 12:52:35 -0800

    Organize and update prediction code

---
+++ paleorec/create_training_data.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 19 15:39:08 2021
+
+@author: shrav
+"""
+
+import pandas as pd
+import numpy as np
+import collections
+from sklearn.utils import resample
+
+'''
+ File to create class - balanced training data from wiki.linkedearth lipd files.
+
+'''
+common_lipdverse_df = pd.read_csv('merged_common_lipdverse_inferred.csv')
+
+
+# MANUAL TASK - TO SCAN THROUGH DATA AND CHECK WHICH FIELDS ARE ACTUALLY SIMILAR BUT HAVE BEEN ENTERED INCORRECTLY
+common_lipdverse_df = common_lipdverse_df.replace(np.nan, 'NA', regex=True)
+common_lipdverse_df = common_lipdverse_df.replace('N/A', 'NA', regex=True)
+common_lipdverse_df = common_lipdverse_df.replace('Sr Ca', 'Sr/Ca', regex=True)
+common_lipdverse_df = common_lipdverse_df.replace('Depth_Cm', 'Depth', regex=True)
+common_lipdverse_df = common_lipdverse_df.replace('Depth_M', 'Depth', regex=True)
+common_lipdverse_df = common_lipdverse_df.replace('per mille VPDB', 'per mil VPDB', regex=True)
+common_lipdverse_df = common_lipdverse_df.replace('per mil VPBD', 'per mil VPDB', regex=True)
+common_lipdverse_df = common_lipdverse_df.replace('per mil', 'permil', regex=True)
+common_lipdverse_df = common_lipdverse_df.replace('g/cm^3', 'g/cm3', regex=True)
+common_lipdverse_df = common_lipdverse_df.replace('per cent', 'percent', regex=True)
+common_lipdverse_df = common_lipdverse_df.replace('µmol/mol', 'μmol/mol', regex=True)
+common_lipdverse_df = common_lipdverse_df.replace('mmolmol', 'mmol/mol', regex=True)
+common_lipdverse_df = common_lipdverse_df.replace('deg C', 'degC', regex=True)
+common_lipdverse_df = common_lipdverse_df.replace('Sea Surface D180W', 'Sea Surface D18Osw', regex=True)
+common_lipdverse_df = common_lipdverse_df.replace('d18ow','D18Osw', regex=True)
+common_lipdverse_df = common_lipdverse_df.replace('Surface P','Surface Pressure', regex=True)
+common_lipdverse_df = common_lipdverse_df.replace('sed rate','Sedimentation Rate', regex=True)
+common_lipdverse_df = common_lipdverse_df.replace('d18ocorr','D18Ocorr', regex=True)
+
+final_df = common_lipdverse_df.filter(['archiveType','proxyObservationType', 'units', 'interpretation/variable', 'interpretation/variableDetail', 'inferredVariable', 'inferredVarUnits'], axis=1)
+
+archives_map = {'marine sediment': 'MarineSediment', 'lake sediment': 'LakeSediment', 'glacier ice': 'GlacierIce', 'documents': 'Documents', 'borehole': 'Rock', 'tree': 'Wood', 'bivalve': 'MollusckShell', 'coral': 'Coral', '': '', 'speleothem': 'Speleothem', 'sclerosponge': 'Sclerosponge', 'hybrid': 'Hybrid', 'Sclerosponge': 'Sclerosponge', 'Speleothem': 'Speleothem', 'Coral': 'Coral', 'MarineSediment': 'MarineSediment', 'LakeSediment': 'LakeSediment', 'GlacierIce': 'GlacierIce', 'Documents': 'Documents', 'Hybrid': 'Hybrid', 'MolluskShell': 'MolluskShell', 'Lake': 'Lake'}
+
+for i, row in final_df.iterrows():
+    final_df.at[i,'archiveType'] = archives_map[row[0]] if row[0] in archives_map else row[0] 
+
+final_df = final_df[final_df.units != 'Mg/Ca']
+    
+counter_arch = collections.Counter(final_df['archiveType'])
+counter_proxy = collections.Counter(final_df['proxyObservationType'])
+counter_units = collections.Counter(final_df['units'])
+counter_int_var = collections.Counter(final_df['interpretation/variable'])
+counter_int_det = collections.Counter(final_df['interpretation/variableDetail'])
+counter_inf_var = collections.Counter(final_df['inferredVariable'])
+counter_inf_var_units = collections.Counter(final_df['inferredVarUnits'])
+
+counter_proxy_a = {key:value for key,value in dict(counter_proxy).items() if value > 5}
+counter_int_var_a = {key:value for key,value in dict(counter_int_var).items() if value > 1}
+counter_int_det_a = {key:value for key,value in dict(counter_int_det).items() if value > 1}
+
+# MANAUL TASK - SCAN THROUGH ALL THE COUNTER VARIABLES TO CHECK IF WE NEED TO OMIT ANY-CO-k( WHERE K CAN BE 1-5, DEPENDING ON THE USE-CASE)
+
+# discard set for proxyObservationType
+discard_set = set(counter_proxy.keys()).difference(set(counter_proxy_a.keys()))
+final_df = final_df[~final_df['proxyObservationType'].isin(discard_set)]
+
+# discard set for interpretation variable
+discard_set = set(counter_int_var.keys()).difference(set(counter_int_var_a.keys()))
+final_df = final_df[~final_df['interpretation/variable'].isin(discard_set)]
+
+# discard set for interpretation variable detail
+discard_set = set(counter_int_det.keys()).difference(set(counter_int_det_a.keys()))
+final_df = final_df[~final_df['interpretation/variableDetail'].isin(discard_set)]
+
+# MANUAL TASK - REMOVE INFERRED VARIABLE TYPES RELATED TO AGE AND OTHER MEASURED VARIABLE TYPE VALUES ENTERED IN THE WIKI
+# discard set for inferredVariableType
+discard_set = {'Calendar Age', 'Accumulation rate', 'k37 n toc', 'acc rate k37', 'acc rate toc', 'Age', 'Radiocarbon Age', 'mg/ca'}
+final_df = final_df[~final_df['inferredVariable'].isin(discard_set)]
+
+
+# DROP NA VALUES FROM THE ARCHIVE TYPE FIELD AS IT CAN NEVER CONTAIN NA
+final_df = final_df[final_df.archiveType != 'NA']
+final_df.dropna(subset=['archiveType'], inplace=True)
+
+# MANUAL TASK - DECIDE WHICH ARCHIVES NEED TO BE DOWN-SAMPLED
+# CURRENTLY WE ARE ONLY DOING WOOD, BECAUSE WE HAVE AROUND 2000 SAMPLES.
+
+# downsample for archiveType = 'Wood'
+df_wood = final_df[final_df.archiveType=='Wood']
+df_marine_sed = final_df[final_df.archiveType=='MarineSediment']
+discard_set = {'Wood', 'MarineSediment'}
+df_rest = final_df[~final_df['archiveType'].isin(discard_set)]
+
+df_wood_downsampled = resample(df_wood, 
+                                 replace=False,    # sample without replacement
+                                 n_samples=350,     # to match minority class
+                                 random_state=123)  # reproducibility
+
+df_marine_downsampled = resample(df_marine_sed, 
+                                 replace=False,    # sample without replacement
+                                 n_samples=350,     # to match minority class
+                                 random_state=123)  # reproducibility
+
+# MANUAL TASK - ADD DATA FOR WOOD FROM INFERRED VARIABLE TYPE CSV FILE,
+# BECAUSE THERE ARE NO SAMPLES WITH UNITS FOR INFERRED VARIABLE TYPE AND INFERRED VARIABLE TYPE UNITS FOR ARCHIVE = WOOD
+
+wood_inferred_df = pd.read_csv('wood_inferred_data.csv')
+wood_inferred_df = wood_inferred_df.replace(np.nan, 'NA', regex=True)
+final_df_downsampled = pd.concat([df_wood_downsampled, wood_inferred_df, df_marine_downsampled, df_rest])
+
+final_df_downsampled = final_df_downsampled.sample(frac=1, random_state=2021).reset_index(drop=True)
+
+# write back the final training data to create the model.
+final_df_downsampled.to_csv('lipdverse_downsampled.csv', sep = ',', encoding = 'utf-8',index = False)
+
+
+
---
+++ paleorec/mcmodel.py
@@ -0,0 +1,326 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 19 15:34:14 2021
+
+@author: shrav
+"""
+import numpy as np
+import pandas as pd
+import math
+import collections
+import copy
+import json
+import itertools
+
+def fill_q0(in_dict, dict_type):
+    '''
+    Add initial probabilites for all the items in the dataset to intial probability dict
+    eg. items in proxyObservationType, units, interpretation/variable and interpretation/variableDetail
+    
+    Parameters
+    ----------
+    in_dict : dict
+        Initial probability dict - 
+    dict_type : dict
+        Iterate over this dict to add its values to the initial probability dict.
+
+    Returns
+    -------
+    None.
+
+    '''
+    for key in dict_type.keys():
+        if key not in in_dict:
+            in_dict[key] = 0
+        else:
+            in_dict[key] = in_dict[key] + dict_type[key]
+
+def calc_freq_multiple(dataframe_obj, ini_map, *argv):
+    '''
+    Calculate the frequency of items for all the columns in argv.
+    Conditional Probability of last column given all the other columns except the last.
+
+    Parameters
+    ----------
+    dataframe_obj : pandas dataframe
+        Dataframe object containing training data.
+    ini_map : dict
+        Contains all the items to be considered for the model.
+    *argv : list
+        Contains the names for the columns that are being considered for calculating frequency.
+
+    Returns
+    -------
+    counter_dict : dict
+        Containing count for all the items that appear against each item in the last column.
+
+    '''
+    counter_dict = {}
+    for index, row in dataframe_obj.iterrows():
+        in_ini = True
+        for arg in argv:
+            if row[arg] not in ini_map:
+                in_ini = False
+                break
+        
+        if not in_ini:
+            return None
+        
+        arg_ = ",".join([row[arg] for arg in argv[:-1]])
+        last_ = row[argv[-1]]
+        if arg_ not in counter_dict:
+            counter_dict[arg_] = {}
+            counter_dict[arg_][last_] = 1                    
+        else:
+            if last_ not in counter_dict[arg_]:
+                    counter_dict[arg_][last_] = 1
+            else:
+                counter_dict[arg_][last_] += 1
+    return counter_dict
+
+def calc_freq(dataframe_obj, col1, col2, ini_map):
+    '''
+    Calculate the frequency of items in col2 for each item in column 1.
+    Conditional Probability of col2 given column 1
+
+    Parameters
+    ----------
+    dataframe_obj : pandas dataframe
+        Dataframe object containing training data.
+    col1 : str
+        Column for which data is being calculated.
+    col2 : str
+        Column whose count is being taken.
+    ini_map : dict
+        Contains all the items to be considered for the model.
+
+    Returns
+    -------
+    counter_dict : dict
+        Containing count for all the items that appear against each item in col1.
+
+    '''
+    counter_dict = {}
+    for index, row in dataframe_obj.iterrows():
+        if row[col1] in ini_map:
+            if row[col1] not in counter_dict:
+                counter_dict[row[col1]] = {}
+                if row[col2] in ini_map:
+                    counter_dict[row[col1]][row[col2]] = 1
+            else:
+                if row[col2] in ini_map:
+                    if row[col2] not in counter_dict[row[col1]]:
+                        counter_dict[row[col1]][row[col2]] = 1
+                    else:
+                        counter_dict[row[col1]][row[col2]] += 1
+    return counter_dict
+
+def add_extra_keys(all_keys, transition_matrix):
+    '''
+    Add missing items for transition from single key to all items in the dataset.
+
+    Parameters
+    ----------
+    all_keys : set
+        Contains all the items that should be in the transition dict for each item.
+    transition_matrix : dict
+        Transition dict object according to the chain type.
+
+    Returns
+    -------
+    transition_mat : dict
+        Updated dict after adding all the items in the transition dict for each item.
+
+    '''
+    transition_mat = copy.deepcopy(transition_matrix)
+    for key, in_dict in transition_mat.items():
+        in_dict_set = set(in_dict.keys())
+        extra_keys = all_keys.difference(in_dict_set)
+        for e_key in extra_keys:
+            in_dict[e_key] = 0
+    return transition_mat
+
+
+def add_one_smoothing(transition_matrix):
+    '''
+    Add one smoothing to prevent the issue caused due to 0 transition probability from one item to the next.
+    Convert counts to log probabilities
+    Parameters
+    ----------
+    transition_matrix : dict
+        Transition dict for all items.
+
+    Returns
+    -------
+    transition_mat : dict
+        Updated transition dict with log probabilities.
+
+    '''
+    transition_mat = copy.deepcopy(transition_matrix)
+    for key, in_dict in transition_mat.items():
+        len_in_dict = len(in_dict) + sum(list(in_dict.values()))
+        for in_key, in_val in in_dict.items():
+            in_dict[in_key] = math.log((in_val + 1)) - math.log(len_in_dict)
+    return transition_mat
+
+
+
+final_df = pd.read_csv('lipdverse_downsampled.csv')
+final_df = final_df.replace(np.nan, 'NA', regex=True)
+
+counter_archive = collections.Counter(final_df['archiveType'])
+counter_proxy = collections.Counter(final_df['proxyObservationType'])
+counter_units = collections.Counter(final_df['units'])
+counter_int_var = collections.Counter(final_df['interpretation/variable'])
+counter_int_det = collections.Counter(final_df['interpretation/variableDetail'])
+counter_inf_var = collections.Counter(final_df['inferredVariable'])
+counter_inf_var_units = collections.Counter(final_df['inferredVarUnits'])
+
+
+# Creating initial probability dict
+q0_chain1 = {key : value for key, value in counter_archive.items()}
+q0_chain2 = {key : value for key, value in counter_archive.items()}
+
+
+# Add all items to the initial probability dict
+fill_q0(q0_chain1, counter_proxy)
+fill_q0(q0_chain1, counter_units)
+
+fill_q0(q0_chain2, counter_proxy)
+fill_q0(q0_chain2, counter_int_var)
+fill_q0(q0_chain2, counter_int_det)
+fill_q0(q0_chain2, counter_inf_var)
+fill_q0(q0_chain2, counter_inf_var_units)
+
+# add-one smoothing for the initial probability dict
+for key, value in q0_chain1.items():
+    q0_chain1[key] += 1
+    
+for key, value in q0_chain2.items():
+    q0_chain2[key] += 1
+    
+# MANUAL STEP - TO SET THE FREQUENCY OF 'NA' TO BE THE MINIMUM SINCE WE DON'T START WITH 'NA'
+# downsample value for 'NA'
+q0_chain2['NA'] = q0_chain2['SD']
+
+# calculate log probabilities for initial probability dict
+q0_total = sum(list(q0_chain1.values()))
+for key, value in q0_chain1.items():
+    q0_chain1[key] = math.log(value) - math.log(q0_total)
+    
+q0_total = sum(list(q0_chain2.values()))
+for key, value in q0_chain2.items():
+    q0_chain2[key] = math.log(value) - math.log(q0_total)
+    
+# creating transition probabilities dict
+transition_matrix_chain1 = {}
+transition_matrix_chain2 = {}
+
+# counter for archive -> proxyObservationType
+archive_inferredVarType_df = final_df.filter(['archiveType', 'proxyObservationType'], axis=1)
+archive_proxy = calc_freq(archive_inferredVarType_df, 'archiveType', 'proxyObservationType', q0_chain1)
+
+# counter for proxyObservationType -> units
+proxy_units_df = final_df.filter(['proxyObservationType','units'], axis=1)
+proxy_units = calc_freq(proxy_units_df, 'proxyObservationType','units', q0_chain1)
+
+# counter for proxyObservationType -> interpretation/variable
+# proxy_int_df = final_df.filter(['proxyObservationType','interpretation/variable'], axis=1)
+# proxy_int = calc_freq(proxy_int_df, 'proxyObservationType','interpretation/variable', q0_chain2)
+
+# counter for interpretation/variable -> interpretation/variableDetail
+int_var_detail_df = final_df.filter(['interpretation/variable', 'interpretation/variableDetail'], axis=1)
+int_var_detail = calc_freq(int_var_detail_df, 'interpretation/variable', 'interpretation/variableDetail', q0_chain2)
+
+# counter for inferredVariable -> inferredVariableUnits
+inf_var_units_df = final_df.filter(['inferredVariable', 'inferredVarUnits'], axis=1)
+inf_var_units = calc_freq(inf_var_units_df, 'inferredVariable', 'inferredVarUnits', q0_chain2)
+
+transition_matrix_chain1.update(archive_proxy)
+transition_matrix_chain1.update(proxy_units)
+
+transition_matrix_chain2.update(archive_proxy)
+# transition_matrix_chain2.update(proxy_int)
+transition_matrix_chain2.update(int_var_detail)
+transition_matrix_chain2.update(inf_var_units)
+
+# add-one smoothing for transition_probabilities matrix
+q0_chain1_set = set(q0_chain1.keys())
+transition_matrix_chain1 = add_extra_keys(q0_chain1_set, transition_matrix_chain1)
+transition_matrix_chain1 = add_one_smoothing(transition_matrix_chain1)
+
+q0_chain2_set = set(q0_chain2.keys())
+transition_matrix_chain2 = add_extra_keys(q0_chain2_set, transition_matrix_chain2)
+transition_matrix_chain2 = add_one_smoothing(transition_matrix_chain2)
+
+# for interpretation/variable
+# interpretation/variable = P[interpretation/variable | archiveType, proxyObservationType]
+
+arch_pr_int_df = final_df.filter(['archiveType', 'proxyObservationType', 'interpretation/variable'])
+arch_pr_int = calc_freq_multiple(arch_pr_int_df, q0_chain2, 'archiveType', 'proxyObservationType', 'interpretation/variable')
+
+# check all the pairs of archive,proxyObservationType
+combo_arch_proxy = list(itertools.product(list(counter_archive.keys()),list(counter_proxy.keys())))
+
+for tup in combo_arch_proxy:
+    if (',').join(tup) not in arch_pr_int:
+        arch_pr_int[(',').join(tup)] = {}
+
+arch_pr_int_chain2 = add_extra_keys(q0_chain2_set, arch_pr_int)
+arch_pr_int_chain2 = add_one_smoothing(arch_pr_int_chain2)
+
+for key, in_dict in arch_pr_int_chain2.items():
+    arch, proxy = key.split(',')
+    prob_arch_proxy = transition_matrix_chain2[arch][proxy]
+    for in_key, in_val in in_dict.items():
+        in_dict[in_key] = in_val - prob_arch_proxy
+        
+# for inferredVariable
+# inferredVariable = P[inferredVariable | interpretation/variable, interpretation/variableDetail, proxyObservationType]
+# to get Numerator = P(proxy intersect interpretation/variable intersect interpretation/variableDetail intersect inferredVariable)
+
+for_inf_df = final_df.filter(['proxyObservationType', 'interpretation/variable', 'interpretation/variableDetail', 'inferredVariable'])
+for_inf = calc_freq_multiple(for_inf_df, q0_chain2, 'proxyObservationType', 'interpretation/variable', 'interpretation/variableDetail', 'inferredVariable')
+
+# check all the pairs of proxyObservationType, interpretation/variable, interpretation/variableDetail
+combo_proxy_int_vardet = list(itertools.product(list(counter_proxy.keys()),list(counter_int_var.keys()), list(counter_int_det.keys())))
+
+for tup in combo_proxy_int_vardet:
+    if (',').join(tup) not in for_inf:
+        for_inf[(',').join(tup)] = {}
+
+pr_int_var_det_inf_chain2 = add_extra_keys(q0_chain2_set, for_inf)
+pr_int_var_det_inf_chain2 = add_one_smoothing(pr_int_var_det_inf_chain2)
+
+# to get Denominator = P(proxy intersect interpretation/variable intersect interpretation/variableDetail)
+pr_int_var_detail_df = final_df.filter(['proxyObservationType', 'interpretation/variable', 'interpretation/variableDetail'])
+pr_int_var_detail = calc_freq_multiple(pr_int_var_detail_df, q0_chain2, 'proxyObservationType', 'interpretation/variable', 'interpretation/variableDetail')
+
+# check all the pairs of proxyObservationType, interpretation/variable, interpretation/variableDetail
+combo_proxy_int_var = list(itertools.product(list(counter_proxy.keys()),list(counter_int_var.keys())))
+
+for tup in combo_proxy_int_var:
+    if (',').join(tup) not in pr_int_var_detail:
+        pr_int_var_detail[(',').join(tup)] = {}
+
+pr_int_var_det_chain2 = add_extra_keys(q0_chain2_set, pr_int_var_detail)
+pr_int_var_det_chain2 = add_one_smoothing(pr_int_var_det_chain2)
+
+for key, in_dict in pr_int_var_det_inf_chain2.items():
+    pr, int_var, int_var_det = key.split(',')
+    prob_proxy_int_var_det = pr_int_var_det_chain2[(',').join([pr, int_var])][int_var_det]
+    for in_key, in_val in in_dict.items():
+        in_dict[in_key] = in_val - prob_proxy_int_var_det
+
+
+transition_matrix_chain2.update(arch_pr_int_chain2)
+transition_matrix_chain2.update(pr_int_var_det_inf_chain2)
+
+# inferredVariable = P[inferredVariable | interpretation/variable, interpretation/variableDetail, proxyObservationType]
+
+archives_map = {'marine sediment': 'MarineSediment', 'lake sediment': 'LakeSediment', 'glacier ice': 'GlacierIce', 'documents': 'Documents', 'borehole': 'Rock', 'tree': 'Wood', 'bivalve': 'MollusckShell', 'coral': 'Coral', '': '', 'speleothem': 'Speleothem', 'sclerosponge': 'Sclerosponge', 'hybrid': 'Hybrid', 'Sclerosponge': 'Sclerosponge', 'Speleothem': 'Speleothem', 'Coral': 'Coral', 'MarineSediment': 'MarineSediment', 'LakeSediment': 'LakeSediment', 'GlacierIce': 'GlacierIce', 'Documents': 'Documents', 'Hybrid': 'Hybrid', 'MolluskShell': 'MolluskShell', 'Lake': 'Lake'}
+model_dict = {'archive_types': list(counter_archive.keys()),'proxy_obs_types': list(counter_proxy.keys()),'units': list(counter_units.keys()),'int_var': list(counter_int_var.keys()),'int_var_det': list(counter_int_det.keys()), 'inf_var': list(counter_inf_var.keys()), 'inf_var_units': list(counter_inf_var_units.keys()), 'archives_map':archives_map, 'q0_chain1' : q0_chain1, 'q0_chain2' : q0_chain2, 'transition_matrix_chain1' : transition_matrix_chain1, 'transition_matrix_chain2' : transition_matrix_chain2}
+
+# write model to file 
+with open('model.txt', 'w') as json_file:
+  json.dump(model_dict, json_file)
